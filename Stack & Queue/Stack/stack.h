1562b71bfb1a27594597267187dd95f0bde395f7
sameer-js <57519299+sameer-js@users.noreply.github.com>
2019-12-05 09:49:51 +0545

    Update stack.h

--- Stack & Queue/Stack/stack.h
+++ Stack & Queue/Stack/stack.h
@@ -1,1 +1,21 @@
+#ifndef STACK_H
+#define STACK_H
+#define MAX 100
 
+class Stack{
+private:
+  int elements[MAX];
+  int tope;
+public:
+  Stack(){tope = -1;};
+  ~Stack(){};
+
+  bool isEmpty();     //check if the stack is empty
+  void push(int element); //inserts element into stack
+  void show();
+  int push();
+  int top();
+  int pop();          //remove the top element
+};
+
+#endif //STACK_H
