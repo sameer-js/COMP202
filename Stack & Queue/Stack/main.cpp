404dbc37200bc1225bd411425613809534970893
sameer-js <57519299+sameer-js@users.noreply.github.com>
2019-12-05 09:48:48 +0545

    Update main.cpp

--- Stack & Queue/Stack/main.cpp
+++ Stack & Queue/Stack/main.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include "stack.h"
+#include "stack.cpp"
+using namespace std;
 
+int main() {
+
+  Stack S;
+  S.push(5);
+  S.push(69);
+  S.push(420);
+  S.show();
+  return 0;
+}
